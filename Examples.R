9a54881028ba4243f719e6aee68b80f2f2f4ee29
naveen-sekhar <naveensekhar06@gmail.com>
2025-07-05 14:25:07 +0530

    Arithmetic operators

---
+++ Examples.R
@@ -0,0 +1,37 @@
+# Arithmetic and String Operations in R
+# Addition
+# Division
+# Square Root
+# Character Concatenation
+#----------------------------
+
+# Addition
+a <- 10
+b <- 20
+c <- a + b
+c
+
+# Division
+d <- 5
+e <- 2
+f <- d / e
+f
+
+# Square root
+g <- sqrt(4)
+g
+
+h <- sqrt(16)
+h
+
+# Character Concatenation
+h <- "Hello"
+i <- "Bob"
+j <- paste(h, i)
+j
+
+k <- paste(h, i, sep = ", ")
+k
+
+l <- paste("Today is", "a good", "day.")
+l
